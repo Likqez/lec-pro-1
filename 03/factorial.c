bd7735e03895b8574c40e47d645b44df6f9ea526
Lukas Lanzner <LanznerLukas@dotspace.dev>
2023-12-05 11:39:22 +0100

    implement missing edge-cases 03/factorial.c

--- 03/factorial.c
+++ 03/factorial.c
@@ -13,18 +13,19 @@ int input_factorial() {
     return input;
 }
 
-long long calculation_factorial(const long long number, const int factor) {
+long long calculation_factorial(const unsigned long long number, const int factor) {
+    if (number == 1) return 1;
     if (factor == 1) return number;
     return calculation_factorial(number * factor, factor - 1);
 }
 
 void output_factorial(const int input, const long long value) {
-    printf("%d! = %lld",input, value);
+    printf("%d! = %lld", input, value);
 }
 
 int main_factorial() {
     const int inputNumber = input_factorial();
-    const long long result = calculation_factorial(inputNumber,inputNumber-1);
+    const long long result = calculation_factorial(inputNumber, inputNumber - 1);
     output_factorial(inputNumber, result);
     return 0;
 }
