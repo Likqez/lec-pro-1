376416ac07b47c337e720c8c2987159761de93d1
Lukas Lanzner <LanznerLukas@dotspace.dev>
2023-12-05 10:40:25 +0100

    03/resistance.c

---
+++ 03/resistance.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+//
+// Created by lanzn on 05.12.2023.
+//
+float input_resistance(char text[]) {
+    printf("%s", text);
+    int input;
+    scanf("%d", &input);
+    printf("\n");
+    return input;
+}
+
+int main_resistance() {
+    // Ask for Resistance of 2 resitors
+    const float r1 = input_resistance("Enter resitance of resistor 1:");
+    const float r2 = input_resistance("Enter resitance of resistor 2:");
+
+    const float total_resistance = 1 / (1 / r1 + 1 / r2);
+
+    printf("The total resistance is %.4E", total_resistance);
+
+    return 0;
+}
