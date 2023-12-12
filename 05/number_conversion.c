7c4cd81efbfa17ff9981f7fe58ad4ce2975c7ecd
Lukas Lanzner <LanznerLukas@dotspace.dev>
2023-12-12 08:23:27 +0100

    number_conversion.c

---
+++ 05/number_conversion.c
@@ -0,0 +1,22 @@
+//
+// Created by lanzn on 12.12.2023.
+//
+
+#include <stdio.h>
+
+void convert_to_binary(int decimal_number);
+
+int main_conversion() {
+    int input;
+    printf("Enter a number:");
+    scanf("%i", &input);
+    convert_to_binary(input);
+    return 0;
+}
+
+void convert_to_binary(int decimal_number) {
+    if (decimal_number != 0) {
+        printf("%i", decimal_number % 2);
+        convert_to_binary(decimal_number/2);
+    }
+}
