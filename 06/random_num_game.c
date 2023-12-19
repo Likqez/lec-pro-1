800e8b0e417846544694f141dbfb5152514b65d9
Lukas Lanzner <LanznerLukas@dotspace.dev>
2023-12-19 11:33:54 +0100

    random_num_game.c

--- 06/random_num_game.c
+++ 06/random_num_game.c
@@ -35,7 +35,7 @@ void ask_multi_or_single() {
 }
 
 void play_multi(const int random_number) {
-    while (p1_guess != random_number && p2_guess != random_number) {
+    while (p1_guess != random_number || p2_guess != random_number) {
         if (turn == 0) {
             printf("\nPlayer 1's guess:");
             scanf("%i", &p1_guess);
@@ -52,7 +52,7 @@ void play_multi(const int random_number) {
             printf("Player 2 wins with %i guesses\n", p2_count);
             exit(0);
         }
-        if(!turn) {
+        if (!turn) {
             printf("%s", p1_guess > random_number ? "Too high" : "Too low");
         } else printf("%s", p2_guess > random_number ? "Too high" : "Too low");
         turn ^= 1;
@@ -70,10 +70,11 @@ void play_single(const int random_number) {
     }
 }
 
-int main_rand_game() {
+int main() {
     ask_multi_or_single();
-    if(mode == 2) {
-        min = 1; max = 100;
+    if (mode == 2) {
+        min = 1;
+        max = 100;
     } else read_min_max();
     const int random_number = get_random_number_for_game(min, max);
     if (mode == 2) {
