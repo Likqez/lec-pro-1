fe670b37fb9a565e9a8bf9a9db205621dcc0410a
Lukas Lanzner <LanznerLukas@dotspace.dev>
2024-01-19 08:30:11 +0100

    file_cards.c

--- 06/file_cards.c
+++ 06/file_cards.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#define MAX_SIZE 5
 //
 // Created by lanzn on 19.12.2023.
 //
@@ -13,6 +14,8 @@ typedef struct card {
     struct tm date_of_birth;
 } CARD;
 
+CARD cards[MAX_SIZE];
+
 void print_card_struc(CARD card) {
     printf("ID: %d\n", card.id);
     printf("Name: %s\n", card.name);
@@ -24,17 +27,54 @@ void print_card_struc(CARD card) {
     printf("Date of birth: %d.%d.%d\n", card.date_of_birth.tm_mday, card.date_of_birth.tm_mon + 1,
            card.date_of_birth.tm_year + 1900);
 }
-CARD card;
 
-int main_file_card_struct() {
-    card.id = 1;
-    strcpy(card.name, "Lukas");
-    strcpy(card.surname, "Lanzner");
-    card.age = 19;
-    card.height = 192;
-    card.weight = 80;
-    strcpy(card.eye_color, "brown");
-    card.date_of_birth = (struct tm) {0, 0, 0, 24, 8, 2004-1900};
-    print_card_struc(card);
+void print_cards(CARD cards[], int size) {
+    for(int i = 0; i < size; i++) {
+        print_card_struc(cards[i]);
+    }
+}
+
+CARD input_card() {
+    CARD card;
+    printf("Enter ID: ");
+    scanf("%d", &card.id);
+
+    printf("Enter Name: ");
+    scanf("%20s", card.name);
+
+    printf("Enter Surname: ");
+    scanf("%20s", card.surname);
+
+    printf("Enter Age: ");
+    scanf("%d", &card.age);
+
+    printf("Enter Height: ");
+    scanf("%d", &card.height);
+
+    printf("Enter Weight: ");
+    scanf("%d", &card.weight);
+
+    printf("Enter Eye color: ");
+    scanf("%10s", card.eye_color);
+
+    printf("Enter Date of birth (dd mm yyyy): ");
+    scanf("%d %d %d", &card.date_of_birth.tm_mday, &card.date_of_birth.tm_mon, &card.date_of_birth.tm_year);
+    card.date_of_birth.tm_mon -= 1; // tm_mon is in the range [0, 11]
+    card.date_of_birth.tm_year -= 1900; // tm_year is the number of years since 1900
+
+    return card;
+}
+
+
+int main() {
+    char continue_input = 'y';
+    for(int i = 0; i < MAX_SIZE; i++) {
+        cards[i] = input_card();
+        printf("Do you want to continue? (y/n): ");
+        scanf(" %c", &continue_input);
+        if(continue_input == 'n') {
+            break;
+        }
+    }
     return 0;
 }
